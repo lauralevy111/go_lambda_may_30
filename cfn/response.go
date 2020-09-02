acf1df27b77a578634c749cef057ef91671610e0
Laura Levy <llevy510@gmail.com>
2020-09-02 17:41:29 -0400

    uncommented err bc we need that later'

--- cfn/response.go
+++ cfn/response.go
@@ -61,8 +61,8 @@ func (r *Response) sendWith(client httpClient) error {
 		return err
 	}
 	req.Header.Del("Content-Type")
-/*
-	res, err := client.Do(req)
+
+	 res, err := client.Do(req)/*
 	if err != nil {
 		log.Printf("client.Do(req)")
 		return err
